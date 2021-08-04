df64122a4cee698269e7bf7e2d8e86e9d7562f01
gitUser203030 <gitUser2030@outlook.com>
2021-08-04 13:34:58 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestProjectGitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
